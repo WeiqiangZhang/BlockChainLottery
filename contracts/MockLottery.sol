0f83e26fefae7ab033543cfacfa2a8781c9b06a7
Weiqiang Zhang <weiqiang.zhang@mail.utoronto.ca>
2020-11-07 16:00:34 -0500

    added tests

--- contracts/MockLottery.sol
+++ contracts/MockLottery.sol
@@ -3,45 +3,46 @@ pragma solidity ^0.6.8;
 import "hardhat/console.sol";
 import "./MockToken.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MockLottery {
+contract MockLottery is AccessControl {
 
   using SafeMath for uint;
 
-  address public owner;
+  bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
   mapping(address => bool) public managers;
   MockToken public tokenContract;
   uint public ticketPrice;
   address[] public players;
   uint public prizePool;
   uint public usagePool;
+  uint public lastSelected;
 
-  constructor(MockToken _tokenContract, uint _ticketPrice) public {
-    owner = msg.sender;
+  constructor(MockToken _tokenContract, uint _ticketPrice, address manager1, address manager2) public {
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _setupRole(MANAGER_ROLE, manager1);
+    _setupRole(MANAGER_ROLE, manager2);
     tokenContract = _tokenContract;
     ticketPrice = _ticketPrice;
     prizePool = 0;
     usagePool = 0;
+    lastSelected = 0;
   }
 
   modifier restricted() { 
-    require(msg.sender == owner); 
+    require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not the owner"); 
     _; 
   }
 
-  function addManager(address _manager) public restricted  {
-    managers[_manager] = true;
+  function addManager(address _manager) public {
+    grantRole(MANAGER_ROLE, _manager);
   }
   
-  function removeManager(address _manager) public restricted  {
-    delete managers[_manager];
+  function removeManager(address _manager) public {
+    revokeRole(MANAGER_ROLE, _manager);
   }
 
-  function setOwner(address _owner) public restricted  {
-    owner = _owner;
-  }
-
-  function setTicketPrice(uint _price) public restricted  {
+  function setTicketPrice(uint _price) public restricted {
     require(players.length == 0);
     ticketPrice = _price;
   }
@@ -50,26 +51,31 @@ contract MockLottery {
     require(_numTickets >= 1);
     uint price = _numTickets.mul(ticketPrice);
     tokenContract.transferFrom(msg.sender, address(this), price);
-    prizePool.add(price);
+    prizePool += (price);
     for (uint i; i < _numTickets; i++) {
       players.push(msg.sender);
     }
   }
 
+  function random() private returns (uint pseudoRandom) {
+    return uint(keccak256(abi.encodePacked(now, block.difficulty, players)));
+  }
+
   function selectWinner() public {
-    require(msg.sender == owner || managers[msg.sender]);
-    uint pseudoRandom = uint(keccak256(abi.encodePacked(now, block.difficulty, players)));
-    uint index = pseudoRandom % players.length;
-    uint winnerPool = prizePool.div(20);
+    require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender) || hasRole(MANAGER_ROLE, msg.sender));
+    require((now - lastSelected).div(60).div(60) >= 5); // Can only select a winner every 5 minutes
+    uint index = random() % players.length;
+    uint winnerPool = prizePool.mul(95).div(100);
     prizePool.sub(winnerPool);
     uint usageFee = prizePool;
     prizePool = 0;
+    lastSelected = now;
     tokenContract.transfer(players[index], winnerPool);
-    usagePool.add(usageFee);
+    delete players;
+    usagePool += usageFee;
   }
 
-  function withdrawUsagePool() public {
-    require(msg.sender == owner);
-    tokenContract.transfer(owner, usagePool);
+  function withdrawUsagePool() public restricted {
+    tokenContract.transfer(msg.sender, usagePool);
   }
 }
--- test/mockLotteryTest.js
+++ test/mockLotteryTest.js
@@ -12,7 +12,7 @@ beforeEach(async function () {
   accounts = await ethers.getSigners();
   await mockToken.deployed();
   MockLottery = await hre.ethers.getContractFactory("MockLottery");
-  mockLottery = await MockLottery.deploy(mockToken.address, 250);
+  mockLottery = await MockLottery.deploy(mockToken.address, 250, accounts[1].address, accounts[2].address);
   await mockLottery.deployed();
 });
 
@@ -20,5 +20,30 @@ describe("MockLottery", function () {
   it("MockLottery should be initialized", async function () {
     assert.ok(mockLottery.address);
   });
+  it("Tickets should initialize with the price of 250", async function () {
+    expect(await mockLottery.ticketPrice()).to.equal(250);
+  });
+  it("Only admin can add manager", async function () {
+    try {
+      await mockLottery.connect(accounts[1]).addManager(accounts[2].address);
+    } catch (err) {
+      assert(true, err)
+    };
+    await mockLottery.addManager(accounts[3].address);
+    expect(await mockLottery.getRoleMemberCount(await mockLottery.MANAGER_ROLE())).to.equal(3);
+    await mockLottery.removeManager(accounts[3].address);
+    expect(await mockLottery.getRoleMemberCount(await mockLottery.MANAGER_ROLE())).to.equal(2);
+  });
+  it("Can set ticket price properly", async function () {
+    await mockLottery.setTicketPrice(69);
+    expect(await mockLottery.ticketPrice()).to.equal(69);
+  });
+  it("Can select winner properly", async function () {
+    await mockToken.connect(accounts[0]).increaseAllowance(mockLottery.address, 1000);
+    await mockLottery.enter(4);
+    expect((await mockToken.balanceOf(accounts[0].address)).toNumber()).to.equal(999000);
+    expect(await mockLottery.prizePool()).to.equal((await mockToken.balanceOf(mockLottery.address)).toNumber());
+    await mockLottery.selectWinner();
+    expect((await mockToken.balanceOf(accounts[0].address)).toNumber()).to.equal(999950);
+  });
 });
-
